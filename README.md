487bd6b3cf749c09bd0731c4ea33a1d0f544dde9
paulinakaras <ppaulina.karas@gmail.com>
2025-03-15 15:44:30 +0100

    Beautifying code

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # javascript-examples
 
 ## Git commands
+
 - `git config user.name "paulinakaras"` - set user name
 - `git config user.email "ppaulina.karas@gmail.com"` - set user mail
 
 ## Terminal commands
 
-### npm commands
+### npm/npx commands
+
 - `npm init` - initialization of js project - package.json file is created - remember to initialize from correct location
 - `npm run hello` - run hello script
+- `npx prettier --write .`- run Prettier
--- exercises/S01/variables.js
+++ exercises/S01/variables.js
@@ -1,14 +1,14 @@
-let varableThatCanBeChanged = 'Hello'
-const constatString = 'World'
+let varableThatCanBeChanged = "Hello";
+const constatString = "World";
 
-let numberVariable = 2
-let stringVariable = 'abc'
-let boolVariable = true
-let otherBoolVariable = false
+let numberVariable = 2;
+let stringVariable = "abc";
+let boolVariable = true;
+let otherBoolVariable = false;
 
-console.log(varableThatCanBeChanged)
-console.log(constatString)
-console.log(varableThatCanBeChanged, constatString)
+console.log(varableThatCanBeChanged);
+console.log(constatString);
+console.log(varableThatCanBeChanged, constatString);
 
-varableThatCanBeChanged = "Goodbye"
-console.log(varableThatCanBeChanged,constatString) 
+varableThatCanBeChanged = "Goodbye";
+console.log(varableThatCanBeChanged, constatString);
