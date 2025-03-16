909196fe0d31877f29c3275a99cbba9f4e9fbb90
paulinakaras <ppaulina.karas@gmail.com>
2025-03-16 15:43:06 +0100

    Add loops

---
+++ exercises/S01/loops.js
@@ -0,0 +1,21 @@
+for (let index = 0; index < 3; index++) {
+  console.log("index:", index);
+}
+
+const anArray = [1, 4, "test", false];
+for (let index = 0; index < anArray.length; index++) {
+  const element = anArray[index];
+  console.log("element:", index, element);
+}
+
+anArray.forEach((element) => {
+  console.log("forEach element:", element);
+});
+
+function elementChecker(anArray) {
+  anArray.forEach((element) => {
+    console.log("elementChecker:", element);
+  });
+}
+
+elementChecker([1, 2, 'test'])
