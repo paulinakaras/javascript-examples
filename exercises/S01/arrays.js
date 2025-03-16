ece0ee00cd3d08605afe2b84cc17b5d1a9d8d74b
paulinakaras <ppaulina.karas@gmail.com>
2025-03-16 14:37:26 +0100

    Add arrays

---
+++ exercises/S01/arrays.js
@@ -0,0 +1,22 @@
+let arrayOfNumbers = [1, 2, 3];
+console.log("arrayOfNumbers:", arrayOfNumbers);
+console.log("number of elements in arrayOfNumbers:", arrayOfNumbers.length);
+
+let arrayOfStrings = ["H", "e", "11", "o"];
+console.log("arrayOfStrings:", arrayOfStrings);
+
+let arrayOfMix = ["H", "e", "11", 1, 2, 4];
+console.log("arrayOfMix:", arrayOfMix);
+
+// array with arrays
+let arrayInArray = ["H", "e", ["xyz"], "ll", [1, 2], 4];
+console.log("arrayInArray:", arrayInArray);
+
+console.log("arrayInArray[0]:", arrayInArray[0]);
+console.log("arrayInArray[2]:", arrayInArray[2]);
+console.log("arrayInArray[4][1]:", arrayInArray[4][1]);
+console.log("number of elements in arrayInArray:", arrayInArray.length);
+
+const arrayWithThreeElements = ["abc", "def", "gh"];
+console.log(arrayWithThreeElements);
+console.log(arrayWithThreeElements[1]);
