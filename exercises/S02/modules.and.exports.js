1cd9eec6e8bce332edf7d68826305372e6c1ab7f
paulinakaras <ppaulina.karas@gmail.com>
2025-03-22 17:45:41 +0100

    Add modules and exports

---
+++ exercises/S02/modules.and.exports.js
@@ -0,0 +1,12 @@
+// to import data from another module use import:
+import { sampleArray } from "./sample.module.array.js";
+
+// we can import multiple elements at once:
+// import {
+//   printArrayOnConsoleWithFor,
+//   printArrayOnConsoleWithForeach,
+// } from "./sample.module.functions.js";
+
+console.log("imported sampleArray:", sampleArray);
+// printArrayOnConsoleWithFor(sampleArray);
+// printArrayOnConsoleWithForeach(sampleArray);
